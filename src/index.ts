14b94ac7c13f758125b7ed28d2d1d41924b210cd
Rajath-Acharya <rajath365@gmail.com>
2023-03-20 01:04:43 +0530

    connecting db

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import express from 'express';
 import AuthRouter from './auth';
+import dotenv from 'dotenv';
+import sequelizeConnection from './db/config';
+
+dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.use('/api/v1/', AuthRouter);
 
-app.listen(PORT, () => {
+async function connectDatabase() {
+	try {
+		await sequelizeConnection.sync();
+		console.log('DB connected');
+	} catch(error) {
+		console.log('Failed to connect database' );
+	}
+}
+
+app.listen(PORT, async () => {
+	await connectDatabase();
 	console.log(`Server is listening on port ${PORT}`);
 });
