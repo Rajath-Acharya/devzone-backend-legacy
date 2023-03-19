f31e6952439af2052104f9a0ab5b3e460fb80e3d
Rajath-Acharya <rajath365@gmail.com>
2023-03-19 20:37:42 +0530

    feat: auth route set up

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import AuthRouter from './auth';
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
+
+app.use('/api/v1/', AuthRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
