181616bcbb2a4272d3e01e9061a9a25af3818f2b
Rajath-Acharya <rajath365@gmail.com>
2023-03-20 13:46:45 +0530

    sample model set up

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,13 +1,22 @@
 import {Request, Response} from 'express';
+import { UserModel } from '../models/user.model';
 
 class AuthController {
 
-	static async login(req:Request,res:Response) {
+	static async login(req:Request,res:Response) {		
 		res.json({message: 'Login successful'});
 	}
 
-	static async register(req:Request,res:Response) {
-		res.json({message: 'Register successful'});
+	static async register(req:Request,res:Response) {		
+		const newUser =  await UserModel.create({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password
+		});
+
+		const user = await UserModel.findOne({ where: { email: newUser.email }});
+		user.password = null;
+		res.json(user);
 	}
 
 	static async logout(req:Request,res:Response) {
